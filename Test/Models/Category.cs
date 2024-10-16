6520bbfc929776ced5168952b34984fabb623b93
shang1607 <kakashang96@msn.com>
2024-10-16 12:50:05 +0200

    legger til database

---
+++ Test/Models/Category.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }   
+
+    public DateTime Created { get; set; } = DateTime.Now; 
+}
