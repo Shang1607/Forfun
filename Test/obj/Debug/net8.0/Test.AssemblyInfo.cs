9b0416cf749a95f026265ebb36c7bdec51fd2417
shang1607 <kakashang96@msn.com>
2024-11-09 17:37:04 +0100

    fisket en feil i delete siden, skulle være delete i delete.cshtml filen, men det var deleteconfirmed

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc483f7b40b2e5bf65f6bf3d1c64bec69193184")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b08761c4520687f056aa4663eaee7d36d7fe345")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
